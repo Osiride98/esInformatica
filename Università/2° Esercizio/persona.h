c1ef060c126ae67a3a188f344423ccb40f7e09ae
toky7 <luca.baldiserri@studio.unibo.it>
2023-07-11 17:55:18 +0200

    Aggiunti commenti

--- Università/2° Esercizio/persona.h
+++ Università/2° Esercizio/persona.h
@@ -2,7 +2,10 @@
 #define _PERSONAH_
 #define MAX_CHAR 10  //SCRITTO IN SNAKE CASE (SEMPRE MAIUSCOLO)
 // ALL'INTERNO DI UNA LIBRERIA VANNO DEFINITE LE FUNZIONI MA NON IMPLEMENTATE
+//RICORDA DI INIZIALIZZARE IL *file*.h con #ifndef e #endif
 
+//definisco la struttura "persona" che contiene tre variabili char (nome,cognome,sesso) e una variabile int (eta)
+// il typedef è uno strumento utile per creare alias per i tipi di dato esistenti e l'utilizzo di variabili complesse nel codice
 typedef struct {
     char nome[MAX_CHAR];
     char cognome[MAX_CHAR];
