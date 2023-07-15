b36cc0fe3aa4609605a532ad2c4c0b6f71f97def
toky7 <luca.baldiserri@studio.unibo.it>
2023-07-15 19:10:19 +0200

    ListaOrari

---
+++ Università/Esercizi - Slide12/sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORTH_
+#define _SORTH_
+
+int naive_sort(int v[], int n);
+int bubble_sort(int v[], int n);
+int insertion_sort(int v[], int n);
+int merge_sort(int v[], int n);
+int quick_sort(int v[], int n);
+
+#endif
